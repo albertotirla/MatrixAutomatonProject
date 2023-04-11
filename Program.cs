13b595a0c4f2cb1f23911c69ccec92cfc9d34095
Alberto Tirla <albertotirla@gmail.com>
2023-04-11 13:50:21 +0300

    completely redo the automaton

--- Program.cs
+++ Program.cs
@@ -1,99 +1,127 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+SortedSet<(int, int)> s = new();
+int n;
+Queue<SortedSet<(int, int)>> QA = new();
+Queue<SortedSet<(int, int)>> QD = new();
+int counter = 0;
 Console.Write("n=");
-int n = (int)uint.Parse(Console.ReadLine());
+n = int.Parse(Console.ReadLine());
 int[,] a = new int[n, n];
-Queue<(int, int)> q = new();
-//an ordered set is, as the name implies, a set of items which preserves insertion order, while also upholding all the properties of a set, for example element uniqueness
-//in c#, there's no built-in class which implements an ordered set
-//while I could make one combining a hashmap and a linked list, the code will be too much for this relatively simple program
-//so, I'm using a simple List<T> to store the result-set, manually checking if an element exists before insertion to avoid duplicates.
-//Note: for large result sets, this might be inefficient because after a time, generic searching algorythms are inefficient without hand-tuned optimisations. However, for the data-set I've seen so far about this problem, the performance implications are a non-issue
+// Read matrix a from console
+init_matrix(a, n);
 
-List<int> s = new();
-Console.WriteLine($"reading the elements of the matrix with dimensions({n}, {n})");
-Console.WriteLine("type each element of the matrix on a single line, press enter after each one. Do so untill the program doesn't allow you to enter values anymore");
-for (int i = 0; i < n; i++)
+int i = 0, j = 0;
+Console.WriteLine("S | a | d");
+var FirstA = GasesteAdiacent(new() { (0, 0) });
+var FirstD = GasesteDiagonal(new() { (0, 0) });
+QA.Enqueue(FirstA);
+QD.Enqueue(FirstD);
+PrintAutomatonLine(new() { (0, 0) }, FirstA, FirstD);
+while (counter < 2 * n + 1)
 {
-    for (int j = 0; j < n; j++)
-    {
-        a[i, j] = int.Parse(Console.ReadLine());
-    }
+    var A = QA.Dequeue();
+    var D = QD.Dequeue();
+    var MA = GasesteAdiacent(A);
+    var MD = GasesteDiagonal(A);
+    PrintAutomatonLine(A, MA, MD);
+    QA.Enqueue(MA);
+    QD.Enqueue(MD);
+    counter++;
+    MA = GasesteAdiacent(D);
+    MD = GasesteDiagonal(D);
+    PrintAutomatonLine(D, MA, MD);
+    QA.Enqueue(MA);
+    QD.Enqueue(MD);
+    counter++;
 }
-Console.WriteLine("finished reading matrix!");
-//because the logic presented below works in most cases but this one, I decided to prepopulate the queue with the first indices, as well as the set with the element situated at coordinates (0, 0)
-q.Enqueue((0, 0));
-s.Add(a[0, 0]);
-while (q.Count > 0)
+// Function to map and print the set
+void MapAndPrintSet(SortedSet<(int, int)> set)
 {
-    (int i, int j) = q.Dequeue();
-    Console.WriteLine($"what should happen to element {a[i, j]}? \n Press a for adjacent movement, d for diagonal movement.");
-    string c = Console.ReadLine();
-    switch (c)
+    StringBuilder sb = new("{");
+    foreach (var (row, col) in set)
     {
-        case "a":
-            muta_adiacent(i, j);
-            break;
-        case "d":
-            muta_diagonal(i, j);
-            break;
-        default:
-            Console.WriteLine("invalid character, try again");
-            continue;
+        sb.Append(a[row, col]).Append(", ");
     }
-    Console.WriteLine("operation complete!");
+    sb.Length -= 2; // Remove the trailing comma and space
+    sb.Append("}");
+    Console.Write(sb.ToString());
+}
+//initialize matrix with user input
+void init_matrix(int[,] a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        Console.WriteLine($"type line {i + 1}, then press enter");
+        string[] input = Console.ReadLine().Split(' ');
+        for (int j = 0; j < n; j++)
+        {
+            a[i, j] = int.Parse(input[j]);
+        }
+    }
+    Console.WriteLine("matrix reading finished successfully");
 }
 
-Console.WriteLine("no more items, the matrix has been fully processed. Displaying result set now, press enter to continue");
-Console.ReadKey();
-foreach (var item in s)
+SortedSet<(int, int)> GasesteAdiacent(SortedSet<(int, int)> x)
 {
-    Console.Write($"{item} ");
+    SortedSet<(int, int)> MRet = new();
+    foreach ((int i, int j) in x)
+    {
+        SortedSet<(int, int)> MElem = new();
+        if (j + 1 < n)
+        {
+            MElem.Add((i, j + 1));
+        }
+        if (j - 1 >= 0)
+        {
+            MElem.Add((i, j - 1));
+        }
+        if (i + 1 < n)
+        {
+            MElem.Add((i + 1, j));
+        }
+        if (i - 1 >= 0)
+        {
+            MElem.Add((i - 1, j));
+        }
+        MRet = new SortedSet<(int, int)>(MRet.Union(MElem));
+    }
+    return MRet;
 }
-
-//functions
-void muta_adiacent(int i, int j)
+SortedSet<(int, int)> GasesteDiagonal(SortedSet<(int, int)> x)
 {
-    if (j + 1 < n && !s.Contains(a[i, j + 1]))
+    SortedSet<(int, int)> MRet = new();
+    foreach ((int i, int j) in x)
     {
-        q.Enqueue((i, j + 1));
-        s.Add(a[i, j + 1]);
+        SortedSet<(int, int)> MElem = new();
+        if (i + 1 < n && j + 1 < n)
+        {
+            MElem.Add((i + 1, j + 1));
+        }
+        if (i - 1 >= 0 && j - 1 >= 0)
+        {
+            MElem.Add((i - 1, j - 1));
+        }
+        if (i + 1 < n && j - 1 >= 0)
+        {
+            MElem.Add((i + 1, j - 1));
+        }
+        if (i - 1 >= 0 && j + 1 < n)
+        {
+            MElem.Add((i - 1, j + 1));
+        }
+        MRet = new SortedSet<(int, int)>(MRet.Union(MElem));
     }
-    if (j - 1 >= 0 && !s.Contains(a[i, j - 1]))
-    {
-        q.Enqueue((i, j - 1));
-        s.Add(a[i, j - 1]);
-    }
-    if (i + 1 < n && !s.Contains(a[i + 1, j]))
-    {
-        q.Enqueue((i + 1, j));
-        s.Add(a[i + 1, j]);
-    }
-    if (i - 1 >= 0 && !s.Contains(a[i - 1, j]))
-    {
-        q.Enqueue((i - 1, j));
-        s.Add(a[i - 1, j]);
-    }
+    return MRet;
 }
-void muta_diagonal(int i, int j)
+void PrintAutomatonLine(SortedSet<(int, int)> state, SortedSet<(int, int)> Adjacent, SortedSet<(int, int)> Diagonal)
 {
-    if (i + 1 < n && j + 1 < n && !s.Contains(a[i + 1, j + 1]))
-    {
-        q.Enqueue((i + 1, j + 1));
-        s.Add(a[i + 1, j + 1]);
-    }
-    if (i - 1 >= 0 && j - 1 >= 0 && !s.Contains(a[i - 1, j - 1]))
-    {
-        q.Enqueue((i - 1, j - 1));
-        s.Add(a[i - 1, j - 1]);
-    }
-    if (i + 1 < n && j - 1 >= 0 && !s.Contains(a[i + 1, j - 1]))
-    {
-        q.Enqueue((i + 1, j - 1));
-        s.Add(a[i + 1, j - 1]);
-    }
-    if (i - 1 >= 0 && j + 1 < n && !s.Contains(a[i - 1, j + 1]))
-    {
-        q.Enqueue((i - 1, j + 1));
-        s.Add(a[i - 1, j + 1]);
-    }
+    MapAndPrintSet(state);
+    Console.Write(" | ");
+    MapAndPrintSet(Adjacent);
+    Console.Write(" | ");
+    MapAndPrintSet(Diagonal);
+    Console.WriteLine();
 }
